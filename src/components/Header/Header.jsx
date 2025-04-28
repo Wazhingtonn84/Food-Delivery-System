2ba0899718fcc55dfd64c7151033ef4e6ee3ae8b
Emmanuel <wazhingtonn84@gmail.com>
2025-04-29 00:53:15 +0300

    Basic Components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css"; 
+
+const Header = () => {
+	return (
+		<div className="header">
+			<div className="header-contents">
+				<h2>Order your favourite food here.</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+					labore voluptatem excepturi molestias fugiat eligendi culpa. Alias nam
+					fugit cumque? Neque quisquam labore cum consequatur fugit, doloremque
+					doloribus delectus maxime placeat eius quaerat quam ullam nostrum.
+				</p>
+				<button>View Menu</button>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
